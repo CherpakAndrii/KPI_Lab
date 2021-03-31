0811153aa136b657e7727eac9af8d220ca2da291
Anatoliy <tolxpams@gmail.com>
2021-03-31 21:20:22 +0300

    вродь работает

--- teacher.cpp
+++ teacher.cpp
@@ -8,19 +8,38 @@
 #include "teacher.hpp"
 
 void teacher::addMark(string studentName, int mark) {
-    fstream file1;
-    file1.open(rating.csv);
-    string s;
-    string name;
-    while (!file1.eof()) {
-        getline(file1,s,'\n');
-        name = "";
-        for (int i=0; s[i]!=','; i++) {name += s[i]}
-        if (name != studentName) {
-            cout << "Такого студента не існує";
-        } else {
-            file1 << to_string(mark);
-            cout << "Файл додано успішно";
+    void addMark(string studentName, int mark) {
+        ifstream file1;
+        char *teka1 = "rating.csv";
+        char *teka2 = "rating1.csv";
+        ofstream file2(teka2);
+        file1.open(teka1);
+        string s;
+        string name;
+        bool f = 0;
+        if (file1.is_open()) {
+            while (!file1.eof()) {
+                getline(file1, s);
+                if (s[s.size()-1] == '\r') {
+                    s.erase(s.end()-1);
+                }
+                name = "";
+                for (int i=0; s[i]!=','; i++) {name += s[i];}
+                if (name == studentName) {
+                    s = s + "," + to_string(mark);
+                    f=1;
+                }
+                s = s + '\n';
+                file2 << s;
+            }
+            if (!f) {
+                cout << "Такого студента не існує";
+            } else {
+                cout << "Оцінку додано успішно";
+            }
         }
+        file1.close();
+        int k = rename(teka2, teka1);
+        file2.close();
     }
 }
